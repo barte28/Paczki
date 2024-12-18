3226e13e586e7c73bcc376390a5e44c5c0aa49c2
barte28 <bartek.teuerle@gmail.com>
2024-12-18 12:31:47 +0100

    gotowe

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -20,5 +20,50 @@ namespace WpfApp3
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double cena = 0;
+            if (postcard_RadioButton.IsChecked == true)
+            {
+                cena = 1;
+                imagemodyfikowany_Image.Source = new BitmapImage(new Uri("postcard.png", UriKind.Relative));
+                cena_TextBlock.Text = "Cena: " + cena + "zł";
+            }
+            if (letter_RadioButton.IsChecked == true)
+            {
+                cena = 1.5;
+                imagemodyfikowany_Image.Source = new BitmapImage(new Uri("letter.png", UriKind.Relative));
+                cena_TextBlock.Text = "Cena: " + cena + "zł";
+            }
+            if (box_RadioButton.IsChecked == true)
+            {
+                cena = 10;
+                imagemodyfikowany_Image.Source = new BitmapImage(new Uri("box.png", UriKind.Relative));
+                cena_TextBlock.Text = "Cena: " + cena + "zł";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string kodPocztowy = kodPocztowy_TextBOx.Text;
+            int kodPocztowyInt;
+
+            if (int.TryParse(kodPocztowy, out kodPocztowyInt))
+            {
+                if (kodPocztowy.Length == 5)
+                {
+                    MessageBox.Show("Dane przesyłki zostały wprowadzone");
+                }
+                else
+                {
+                    MessageBox.Show("Kod pocztowy musi zawierać 5 cyfr");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Kod pocztowy musi być liczbą");
+            }
+        }
     }
 }
